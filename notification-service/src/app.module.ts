6e82797f0e30132799dd245452a37469ff74d205
Tamjid Ahmed <tamjidahmed050@gmail.com>
2026-05-02 00:40:19 +0600

    chore(notification): add notification service module setup

---
+++ notification-service/src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { NotificationModule } from './notification/notification.module';
+
+
+@Module({
+  imports: [NotificationModule]
+})
+export class AppModule {}
