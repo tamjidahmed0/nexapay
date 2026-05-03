c4a35445012764e77a493a12393d5275d3056ad2
Tamjid Ahmed <tamjidahmed050@gmail.com>
2026-05-03 18:48:50 +0600

    fix: payment-service crash in container

--- gateway/src/redis/redis.module.ts
+++ gateway/src/redis/redis.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { RedisService } from './redis.service';
 
 @Module({
-    imports: [ConfigModule],
+    imports: [],
     providers: [RedisService],
     exports: [RedisService],
 })
